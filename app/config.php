1c1ec27c89777f1f9d96e4271f9178e4f3bc5425
6hislain <ghislainmike80@gmail.com>
2021-06-08 11:42:27 +0200

    display PHP errors & .htaccess update

--- app/config.php
+++ app/config.php
@@ -1,4 +1,8 @@
 <?php
+ini_set("display_errors", 1); // * display all errors
+ini_set("display_startup_errors", 1);
+error_reporting(E_ALL);
+
 define('APP_KEY', 'iamnotthesecretkey'); // * needed by authenticate.php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
